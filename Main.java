bc11138a61e416f8e314d89de2288440a62af444
Owen Poole <owen.poole@live.com>
2023-10-08 23:20:00 -0700

    update'

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main {
   public static void main(String[] args) {
 
     JButton button1 = GUIFrame.Button("One Player", 175, 97, 150, 30);
-    JButton button2 = GUIFrame.Button("Two Player", 175, 175, 150, 30);
+    JButton button2 = GUIFrame.Button("Two Players", 175, 175, 150, 30);
     
     new GUIFrame(500, 300, button1, button2);
     
